fe190797d24f088d5b53df35d1692e8d2139099e
Oskari Okko Ojala <git@okko.fi>
2022-02-13 23:25:40 +0200

    Implement vw api

--- lib/server.ts
+++ lib/server.ts
@@ -1,12 +1,9 @@
 import express from 'express';
+const path = require('path');
 const app = express();
 const PORT = 8000;
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
-});
 
-const VwWeconnect = require('./VwWeconnect/main.js');
+import VwWeconnect from 'iobroker.vw-connect';
 const connect = VwWeconnect()
 connect.sendEvent('ready')
 
@@ -21,6 +18,22 @@ app.get('/vwstatus', (_req, res) => {
         'currentSOC_pct': state[vin + '.status.batteryStatus.currentSOC_pct'],
         'cruisingRangeElectric_km': state[vin + '.status.batteryStatus.cruisingRangeElectric_km'],
         'plugConnectionState': state[vin + '.status.plugStatus.plugConnectionState'],
+        'chargingState': state[vin + '.status.chargingStatus.chargingState'],
+        'chargePower_kW': state[vin + '.status.chargingStatus.chargePower_kW'],
+        'chargeRate_kmph': state[vin + '.status.chargingStatus.chargeRate_kmph'],
+        'remainingChargingTimeToComplete_min': state[vin + '.status.chargingStatus.remainingChargingTimeToComplete_min'],
+        'chargingStatusCarCapturedTimestamp': state[ vin + '.status.chargingStatus.carCapturedTimestamp'],
         ...state,
     })
 })
+
+app.use(express.static(path.resolve(__dirname, './../build')));
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './../build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,44 @@
 import './App.css';
+import React from "react";
 import Gauge from './Gauge';
 import Charge from './Charge';
 
+const INTERVAL_MS = 1000;
 function App() {
+  const [data, setData] = React.useState(undefined);
+
+  function getVwStatus() {
+    fetch("/vwstatus")
+    .then((res) => res.json())
+    .then((data) => setData(data));
+  }
+
+  function chargingState(state: string | undefined) {
+    switch(state) {
+      case undefined:
+        return '';
+      case 'error':
+        return '❌';
+      case 'charging':
+        return '⚡';
+      default:
+        return state
+    }
+  }
+
+  function chargingSpeed(power: string | undefined, rate: string | undefined) {
+    if (power && rate) {
+      return power + ' kW, ' + rate + ' km/h';
+    } else {
+      return '';
+    }
+  }
+
+  React.useEffect(() => {
+    const timer = setInterval(() => getVwStatus(), INTERVAL_MS);
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <div className="App">
       <header>
@@ -10,10 +46,17 @@ function App() {
         </header>
       <main>
         <div><img src="id4.webp" width="80%" /></div>
-        <div>
-          <Gauge value={60}></Gauge>
-          <div className="km-value-text">270 km</div>
-        </div>
+        {data &&
+          <div>
+            <Gauge value={ data?.['currentSOC_pct'] || 0 }></Gauge>
+            <div className="km-value-text">{ data?.['cruisingRangeElectric_km'] ? data?.['cruisingRangeElectric_km'] + 'km' : '' }</div>
+            <div className="plug-connection-state">{ data?.['plugConnectionState'] === 'connected' ? '🔌✅' : '' }</div>
+            <div className="charging-state">{ chargingState(data?.['chargingState']) }</div>
+            <div className="charging-speed">{ chargingSpeed(data?.['chargePower_kW'], data?.['chargeRate_kmph']) }</div>
+            <div className="charging-remaining">{ data?.['remainingChargingTimeToComplete_min'] || '' }</div>
+            <div className="charging-timestamp">{ data?.['chargingStatusCarCapturedTimestamp'] || '' }</div>
+          </div>
+        }
         <div></div>
       </main>
       <aside>
